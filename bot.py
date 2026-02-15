5b3f948f63b48b2ca8c84599b5300917780a36b0
aryan686 <126113372+aryan686@users.noreply.github.com>
2026-02-15 23:50:35 +0530

    Fix typo in CallbackQueryHandler import

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ from telegram import (
 from telegram.ext import (
     ApplicationBuilder,
     CommandHandler,
-    CallbackQueryHandler,
+    CallbackQuerYHandler,
     MessageHandler,
     ContextTypes,
     filters,
